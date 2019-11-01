ed130fae93cffaf7c61c38e9a30a43104896c6e9
mob-sakai <sakai@mobcast.jp>
2019-11-01 21:47:04 +0900

    bridge test

---
+++ Assets/BridgeTest/Editor/BridgeTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BridgeTest
+{
+
+    public class testtest
+    {
+        [UnityEditor.MenuItem("Packages/BridgeTest")]
+        static void t()
+        {
+            var t = typeof(UnityEditor.PackageManager.UI.InternalBridge);
+            Debug.Log(t);
+            Debug.Log(t.Assembly.FullName);
+            Debug.Log(t.AssemblyQualifiedName);
+            // UnityEditor.PackageManager.UI.Bridge.Test.test();
+            var o = UnityEditor.PackageManager.UI.InternalBridge.AssemblyTest2();
+            Debug.Log(o);
+            Debug.Log(o.GetType());
+            Debug.Log(o.GetType().Assembly.FullName);
+            Debug.Log(o.GetType().AssemblyQualifiedName);
+        }
+    }
+}
---
+++ Assets/Bridges/Editor/NewBehaviourScript.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.PackageManager.UI;
+using LibGit2Sharp;
+
+namespace UnityEditor.PackageManager.UI
+{
+    public class InternalBridge
+    {
+
+
+        public InternalBridge()
+        {
+
+        }
+
+
+        static IEnumerable<Package> GetPackages()
+        {
+            var collection = PackageCollection.Instance;
+            return collection?.LatestListPackages
+                .Select(x => collection.GetPackageByName(x.Name))
+                .Distinct() ?? Enumerable.Empty<Package>();
+        }
+
+        public static void AddCallback(object element, Action action)
+        {
+            var packageList = element as PackageList;
+            packageList.OnLoaded += action;
+        }
+
+
+        public static string GetSelectedPackage()
+        {
+            var collection = PackageCollection.Instance;
+            return collection?.SelectedPackage;
+        }
+
+
+        public static object AssemblyTest()
+        {
+            return new LibGit2Sharp.CloneOptions();
+        }
+
+        public static object AssemblyTest2()
+        {
+            return new PackageInfo(
+                
+            );
+        }
+    }
+}
+
+
