cbee809caab05919fdce1cde889d0f25864d11cb
mob-sakai <sakai@mobcast.jp>
2019-11-11 16:47:46 +0900

    ...

--- Packages/com.coffee.upm-git-extension/Editor/InternalAccessBridge/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-
-
-git subtree split --prefix="Packages/com.coffee.packageA" --branch package-a
-git subtree split --prefix="Packages/com.coffee.packageB" --branch package-b
-git subtree split --prefix="Packages/com.coffee.packageC" --branch package-c
-git subtree split --prefix="Packages/com.coffee.packageX" --branch package-x
---
+++ Packages/com.coffee.upm-git-extension/Editor/InternalAccessBridge/get-available-refs.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+REPO_URL=$1
+DIR=$2
+UNITY=$3
+
+# Create repo.
+if [ ! -e "${DIR}" ]; then
+    mkdir "${DIR}"
+    cd "${DIR}"
+    git init
+    git remote add origin "${REPO_URL}"
+else
+    cd "${DIR}"
+fi
+
+# Clear cache file.
+: > versions
+
+# Fetch all branches/tags.
+git fetch --depth=1 -fq --prune origin 'refs/tags/*:refs/tags/*' '+refs/heads/*:refs/remotes/origin/*'
+for ref in `git show-ref | cut -d ' ' -f 2`
+do
+    echo $ref
+
+	# Check if package.json and package.json.meta exist.
+    git checkout -q $ref -- package.json package.json.meta
+    [ $? != 0 ] && continue
+
+    echo "  -> OK 1"
+
+
+	# Check supported unity versions.
+    SUPPORTED_VERSION=`grep -o -e "\"unity\".*$" package.json | sed -e "s/\"unity\": \"\(.*\)\".*$/\1/"`
+    echo "${SUPPORTED_VERSION} ${UNITY}"
+
+    [[ "${UNITY}" < "${SUPPORTED_VERSION}" ]] && continue
+    echo "  -> OK 2"
+
+	# Output only available names
+    echo $ref >> versions
+done
